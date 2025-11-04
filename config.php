8e33f33a199cfeb27056c0dc26c44591112d64dd
Breno Santos <brenomachado0520@gmail.com>
2025-11-03 22:11:36 -0300

    Atualização de caminho base

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // PATHS and FOLDERS
-define('BASE_PATH','/sistema-estagio/');
+define('BASE_PATH','/sis-estagio/');
 define('BASE_URL','http://localhost' . BASE_PATH);
 define('SIS_NAME','Sistema de Estágio');
 
